758eb092aefdb5d430cdf339da084a9016727cba
antonmashko <a.mashko@maytech.net>
2015-12-29 18:53:08 +0200

    testing on create rules for layout

---
+++ bandura-player/Bandura.ViewModel/PlayerControlWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bandura.ViewModel {
+
+    public partial class PlayerControlWindow : Form {
+
+
+        public PlayerControlWindow() {
+
+        }
+
+
+
+
+
+    }
+}
