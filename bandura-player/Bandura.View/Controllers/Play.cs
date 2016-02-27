4668da10ae168b10ef3f5de60e928020aa84c300
antonmashko <anton.mashko@gmail.com>
2016-02-27 10:10:06 +0200

    upd config

--- bandura-player/Bandura.View/Controllers/Play.cs
+++ bandura-player/Bandura.View/Controllers/Play.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bandura.Design.Controllers {
+﻿namespace Bandura.Design.Controllers {
 
 
     public sealed class Play {
